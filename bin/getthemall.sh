7dd9aa95050e9aec9b427f3cc4669dea4ea237b2
memefulsearch <mauricioprado00+memefulsearch@gmail.com>
2017-08-31 01:25:35 +0200

    add script to update files

---
+++ bin/getthemall.sh
@@ -0,0 +1,9 @@
+for x in $(seq 1 10); do  bash -c "curl 'http://memeful.com/web/ajax/posts?count=2000&page=${x}&tags=' > collect/${x}-2000.json" && sleep 1 ; done
+
+jq -s . collect/*.json > data.json
+
+echo 'var data = ' > data.js
+cat data.json | jq '.[] | .data | .[] | [.imageUrl, .tags]' | jq -s . >> data.js
+echo ';' >> data.js
+
+rm data.json
