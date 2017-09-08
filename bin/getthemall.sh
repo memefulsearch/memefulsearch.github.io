e5ceb270734faaefbc97065ef4e0031033613fde
memefulsearch <mauricioprado00+memefulsearch@gmail.com>
2017-09-08 22:47:15 +0200

    Update data to animated gif images

--- bin/getthemall.sh
+++ bin/getthemall.sh
@@ -6,4 +6,8 @@ echo 'var data = ' > data.js
 cat data.json | jq '.[] | .data | .[] | [.imageUrl, .tags]' | jq -s . >> data.js
 echo ';' >> data.js
 
+# make them animated
+sed -i 's#700w#700wa#g' data.js
+sed -i 's#\.jpg#.gif#g' data.js
+
 rm data.json
